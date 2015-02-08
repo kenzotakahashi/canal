1043404fbecec785a78a28f468a67cc20abced90
Kenzo Takahashi <kenzotakahashi2@gmail.com>
2015-02-08 09:31:04 -0800

    almost ready

--- client/main.js
+++ client/main.js
@@ -8,9 +8,9 @@ Meteor.subscribe("userData");
 Meteor.subscribe("matches");
 Meteor.subscribe("chat");
 
-Template.main.rendered = function(){
-  $('.ui.sidebar').sidebar('attach events', '.text .item');
-};
+
+
+// ================ Chat ==============
 
 Template.chat.helpers({
 	'messages': function(){
@@ -18,8 +18,11 @@ Template.chat.helpers({
 	},
 	'senderName': function(sender){
 		return Meteor.users.findOne(sender).profile.name;
+	},
+	'myLine': function(sender){
+		return !! (sender === Meteor.userId());
 	}
-})
+});
 
 Template.chat.events({
 	'submit form': function(e, template){
@@ -41,6 +44,8 @@ Template.chat.events({
 	}
 });
 
+// ============== Matches ==============================
+
 Template.matches.helpers({
 	'matches': function(){
 		if (Meteor.user().profile.type === 'startup') {
@@ -49,34 +54,224 @@ Template.matches.helpers({
 			return Matches.find({'investor': Meteor.userId()});
 		}
 	},
-
 	'getUser': function(){
-		var type = (Meteor.user().profile.type === 'startup') ? this.investor : this.startup;
-		return Meteor.users.findOne(type);
+		// 'this' is a match object
+		if (Meteor.user().profile.type === 'startup') {
+			return Meteor.users.findOne(this.investor).profile.name;
+		} else {
+			return Meteor.users.findOne(this.startup).profile.companyName;
+		}
 	}
 });
 
-//==============
+Template.matches.events({
+	'click .back': function(){
+		pickUser();
+	},
+});
+
+//============== Occupation ================================
+
+Template.occupation.events({
+	'click .startup': function(){
+		Meteor.call('setOccupation', 'startup', function(error, result) {
+			if (error)
+			  return throwError(error.reason);
+		});
+		Router.go('/companyName');
+	},
+	'click .investor': function(){
+		Meteor.call('setOccupation', 'investor', function(error, result) {
+			if (error)
+			  return throwError(error.reason);
+		});
+		pickUser();
+	}
+
+});
+
+// ============ main =========================================
+
+Template.main.rendered = function(){
+  $('.ui.sidebar').sidebar('attach events', '.text .item');
+};
+
+Template.main.helpers({
+	'isStartup': function(){
+		return !! (Meteor.user().profile.type === 'startup');
+	}
+});
+
+Template.main.events({
+	'click .meet': function(e, template){
+		Meteor.call('meet', template.data._id, function(error, result) {
+			if (error)
+			  return throwError(error.reason);
+			pickUser();
+		});
+	},
+	'click .pass': function(e, template){
+		Meteor.call('pass', template.data._id, function(error, result) {
+			if (error)
+			  return throwError(error.reason);
+			pickUser();
+		});
+	},
+});
+
+// =============== sidebar ==================================
+
+Template.sidebar.events({
+	'click .home': function(e, template){
+	 	$(".ui.sidebar").sidebar("setting", {
+	    onHidden: function () {
+	    	pickUser();
+	    }
+		}).sidebar('hide');		
+	},
+	'click .logout': function(e, template){
+	 	$(".ui.sidebar").sidebar("setting", {
+	    onHidden: function () {
+	    	Meteor.logout();
+	    	Router.go('/');
+	    }
+		}).sidebar('hide');
+	},
+	'click .profile': function(e, template){
+	 	$(".ui.sidebar").sidebar("setting", {
+	    onHidden: function () {
+	    	Router.go('/profile');
+	    }
+		}).sidebar('hide');
+	},
+});
+
+Template.profile.rendered = function(){
+  $('.ui.sidebar').sidebar('attach events', '.text .item');
+};
+
+// ============ Profile ===============================
+
+Template.profile.helpers({
+	'isStartup': function(){
+		return !! (Meteor.user().profile.type === 'startup');
+	}
+});
+
+
+
+// ============ Edit ===================================
+
+Template.edit.helpers({
+	'isStartup': function(){
+		return !! (Meteor.user().profile.type === 'startup');
+	}
+});
+
+Template.edit.events({
+	'submit form': function(e){
+		e.preventDefault();
+		if (Meteor.user().profile.type === 'startup'){
+			profile = {
+				'profile.companyName': $(e.target).find('[name=name]').val(),
+				'profile.location': $(e.target).find('[name=location]').val(),
+				'profile.url': $(e.target).find('[name=url]').val(),
+				'profile.angelList': $(e.target).find('[name=angelList]').val(),
+				'profile.description': $(e.target).find('[name=description]').val(),
+			};
+		} else {
+			profile = {
+				// firstName: $(e.target).find('[name=firstName]').val(),
+				// lastName: $(e.target).find('[name=lastName]').val(),
+				'profile.location': $(e.target).find('[name=location]').val(),
+				'profile.description': $(e.target).find('[name=description]').val(),				
+			};			
+		}
+		Meteor.call('edit', profile, function(error, result) {
+			if (error)
+			  return throwError(error.reason);
+			Router.go('profile');
+		});
+	}
+});
+
+// ============ top =========================
+
+Template.top.events({
+	'click .google': function(e, template){
+		Meteor.loginWithGoogle({requestPermissions: ['email']}, function (error) {
+		  if (error)
+		    console.log(error);
+		  else {
+
+		  	if (Meteor.user().profile.type){
+		  		pickUser();
+		  	} else {
+					Router.go('occupation');
+		  	}
+		  }
+		});
+	}
+});
+
+// ======== CompanyName ======================
+
+Template.companyName.events({
+	'submit form': function(e, template){
+		e.preventDefault();
+		profile = {
+			'profile.companyName': $(e.target).find('[name=name]').val(),
+		};
+		Meteor.call('edit', profile, function(error, result) {
+			if (error)
+			  return throwError(error.reason);
+			pickUser();
+		});
+	}
+});
+
+
+
+
+
+
+
+
+function pickUser(){
+	// Return a user whom the current user has not seen before.
+	var profile = Meteor.user().profile;
+	var type = (profile.type === 'investor') ? 'startup' : 'investor';
+	var user = _.find(Meteor.users.find({'profile.type': type}).fetch(), function(user){
+		return !_.contains(profile.meet.concat(profile.pass), user._id);
+	});
+
+	Router.go('/main/' + ((user) ? user._id : 'no-user'));
+};
+
+
+
+
+
 
 Template.signup.events({
 	'submit form': function(e, template){
 		e.preventDefault();
-
     var user = {
       email: $(e.target).find('[name=email]').val(),
       password: $(e.target).find('[name=password]').val()
     };
-		
+
 		// TODO: validation
 		Accounts.createUser({
 			email: user.email,
 			password: user.password,
 			profile: {
 				type: null,
-				name: null,
+				location: '',
+				description: '',
 				meet: [],
 				pass: [],
-				match: []
+				match: [],
 			}
 		});
 
@@ -99,81 +294,23 @@ Template.login.events({
       	pickUser();
       }
     });
+	},
+	// TODO: dupulication
+	'click .google': function(e, template){
+		Meteor.loginWithGoogle({requestPermissions: ['email']}, function (error) {
+		  if (error)
+		    console.log(error);
+		  else {
 
+		  	if (Meteor.user().profile.type){
+		  		pickUser();
+		  	} else {
+					Router.go('occupation');
+		  	}
+		  }
+		});
 	}
 });
-
-Template.occupation.events({
-	'click .startup': function(){
-		Meteor.call('setOccupation', 'startup', function(error, result) {
-			if (error)
-			  return throwError(error.reason);
-		});
-		pickUser();
-	},
-	'click .investor': function(){
-		Meteor.call('setOccupation', 'investor', function(error, result) {
-			if (error)
-			  return throwError(error.reason);
-		});
-		pickUser();
-	}
-
-});
-
-Template.main.events({
-	'click .meet': function(e, template){
-		Meteor.call('meet', template.data._id, function(error, result) {
-			if (error)
-			  return throwError(error.reason);
-			pickUser();
-		});
-	},
-	'click .pass': function(e, template){
-		Meteor.call('pass', template.data._id, function(error, result) {
-			if (error)
-			  return throwError(error.reason);
-			pickUser();
-		});
-	},
-	'click .home': function(e, template){
-	 	$(".ui.sidebar").sidebar("setting", {
-	    onHidden: function () {
-	    	pickUser();
-	    }
-		}).sidebar('hide');		
-	},
-	'click .logout': function(e, template){
-	 	$(".ui.sidebar").sidebar("setting", {
-	    onHidden: function () {
-	    	Meteor.logout();
-	    	Router.go('/');
-	    }
-		}).sidebar('hide');
-	},
-
-});
-
-
-function pickUser(){
-	// Return a user whom the current user has not seen before.
-	var profile = Meteor.user().profile;
-	var type = (profile.type === 'investor') ? 'startup' : 'investor';
-	var user = _.find(Meteor.users.find({'profile.type': type}).fetch(), function(user){
-		return !_.contains(profile.meet.concat(profile.pass), user._id);
-	});
-	Router.go('/main/' + user._id);
-};
-
-
-
-
-
-
-
-
-
-
 
 
 
--- server/server.js
+++ server/server.js
@@ -16,6 +16,41 @@ Meteor.publish("userData", function (){
   return Meteor.users.find();
 });
 
+ServiceConfiguration.configurations.upsert(
+  { service: "google" },
+  {
+    $set: {
+      clientId: "88213826089-cnjhm0vtu513eb4mj3tsbvu4qbpur1bg.apps.googleusercontent.com",
+      loginStyle: "popup",
+      secret: "7zqd_l94_8D3ICBXJp8x2Yh1"
+    }
+  }
+);
+
+function merge_options(obj1,obj2){
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+}
+
+Accounts.onCreateUser(function(options, user) {
+  // We still want the default hook's 'profile' behavior.
+  if (options.profile)
+		profile = {
+			type: null,
+			location: '',
+			description: '',
+			meet: [],
+			pass: [],
+			match: [],
+		}
+
+    user.profile = merge_options(profile, options.profile);
+
+  return user;
+});
+
 
 if (Meteor.users.find().count() === 0){
 	for (var i = 0; i < 10; i++){
@@ -25,6 +60,8 @@ if (Meteor.users.find().count() === 0){
 			profile: {
 				type: 'investor',
 				name: 'investor ' + i,
+				location: 'San Francisco',
+				description: "I'm an Internet guy who believes in the triumph of humanity with a little help from technology. My current job is CEO of Jelly",
 				meet: [],
 				pass: [],
 				match: []
@@ -38,7 +75,11 @@ if (Meteor.users.find().count() === 0){
 			password: 'pass' + i,
 			profile: {
 				type: 'startup',
-				name: 'startup ' + i,
+				companyName: 'startup ' + i,
+				location: 'San Francisco',
+				description: 'Dropbox is a free service that lets you bring your photos, docs, and videos anywhere and share them easily. Dropbox was founded in 2007 by Drew Houston and Arash Ferdowsi, two MIT students tired of emailing files to themselves to work from more than one computer.',
+				url: 'http://dropbox.com',
+				angelList: 'https://angel.co/dropbox',
 				meet: [],
 				pass: [],
 				match: []				
